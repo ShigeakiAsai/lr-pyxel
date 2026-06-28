1fcada1d4bad0693727d16ad2af0ab42235d952a
Shigeaki Asai <y-asai.0816@docomonet.jp>
2026-06-28 18:05:58 +0900

    Fix: Able to start with no contents more and more and more and more and more

--- src/lib.rs
+++ src/lib.rs
@@ -15,8 +15,8 @@ const COLOR_WHITE: u16 = 0xFFFF;
 // RETRO_ENVIRONMENT_SHUTDOWN asks the frontend to close the core
 const ENVIRONMENT_SHUTDOWN: c_uint = 7;
 
-// RETRO_ENVIRONMENT_SET_SUPPORT_NO_GAME allows the core to run without any content
-const ENVIRONMENT_SET_SUPPORT_NO_GAME: c_uint = 17;
+// Fixed: Changed from 17 to 18 to correctly match the Libretro API specification
+const ENVIRONMENT_SET_SUPPORT_NO_GAME: c_uint = 18;
 
 #[no_mangle]
 pub unsafe extern "C" fn retro_get_system_info(info: *mut c_void) {
@@ -24,26 +24,23 @@ pub unsafe extern "C" fn retro_get_system_info(info: *mut c_void) {
     (*info).library_name = b"Pyxel\0".as_ptr() as *const c_char;
     (*info).library_version = b"0.1.0\0".as_ptr() as *const c_char;
     
-    // Set extensions to empty string to allow "Start Core" without any ROM file.
+    // Fixed: Set extensions to empty and need_fullpath to true to ensure "Start Core" behaves correctly
     (*info).valid_extensions = b"\0".as_ptr() as *const c_char;
-    
-    // Set to false so RetroArch does not require a ROM file path.
-    (*info).need_fullpath = false;
+    (*info).need_fullpath = true;
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn retro_set_environment(cb: unsafe extern "C" fn(c_uint, *mut c_void) -> bool) {
+pub unsafe extern "C" fn retro_set_environment(cb: unsafe extern "C" fn(c_uint, *mut c_void) -> bool) -> bool {
     ENVIRON_CB = Some(cb);
-
-    // Must be called first, before any other environment calls.
-    // Use u8 instead of bool to match the C ABI representation reliably.
-    let mut supported: u8 = 1;
-    cb(ENVIRONMENT_SET_SUPPORT_NO_GAME, &mut supported as *mut u8 as *mut c_void);
-
+    
     let format = libretro_sys::PixelFormat::RGB565;
     cb(libretro_sys::ENVIRONMENT_SET_PIXEL_FORMAT, &format as *const _ as *mut c_void);
-}
 
+    let support_no_game = true;
+    cb(ENVIRONMENT_SET_SUPPORT_NO_GAME, &support_no_game as *const _ as *mut c_void);
+
+    true
+}
 
 #[no_mangle]
 pub unsafe extern "C" fn retro_set_video_refresh(cb: unsafe extern "C" fn(*const c_void, c_uint, c_uint, usize)) {
@@ -51,7 +48,26 @@ pub unsafe extern "C" fn retro_set_video_refresh(cb: unsafe extern "C" fn(*const
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn retro_load_game(_game: *const c_void) -> bool {
+pub unsafe extern "C" fn retro_set_audio_sample(_cb: unsafe extern "C" fn(i16, i16)) {}
+#[no_mangle]
+pub unsafe extern "C" fn retro_set_audio_sample_batch(_cb: unsafe extern "C" fn(*const i16, usize) -> usize) -> usize { 0 }
+
+#[no_mangle]
+pub unsafe extern "C" fn retro_set_input_poll(cb: unsafe extern "C" fn()) {
+    INPUT_POLL = Some(cb);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn retro_set_input_state(cb: unsafe extern "C" fn(c_uint, c_uint, c_uint, c_uint) -> i16) {
+    INPUT_STATE = Some(cb);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn retro_load_game(game: *const c_void) -> bool {
+    // Safely allow standalone boot even when game info pointer is null
+    if game.is_null() {
+        return true;
+    }
     true
 }
 
@@ -60,27 +76,27 @@ pub unsafe extern "C" fn retro_run() {
     const WIDTH: usize = 256;
     const HEIGHT: usize = 256;
 
-    // Poll input state from the frontend
-    if let Some(poll) = INPUT_POLL {
-        poll();
+    if let Some(input_poll) = INPUT_POLL {
+        input_poll();
     }
 
-    // Check A button (RETRO_DEVICE_ID_JOYPAD_A = 8)
-    let a_pressed = INPUT_STATE
-        .map(|f| f(0, libretro_sys::DEVICE_JOYPAD, 0, 8))
-        .unwrap_or(0);
-
-    let color = if a_pressed != 0 {
-        // Flash white for one frame, then request shutdown
-        if let Some(env) = ENVIRON_CB {
-            env(ENVIRONMENT_SHUTDOWN, std::ptr::null_mut());
+    let mut select_pressed = false;
+    if let Some(input_state) = INPUT_STATE {
+        // DEVICE_JOYPAD = 1, DEVICE_ID_JOYPAD_A = 0
+        let state = input_state(0, 1, 0, 0);
+        if state != 0 {
+            select_pressed = true;
         }
-        COLOR_WHITE
-    } else {
-        COLOR_GREEN
-    };
+    }
 
-    let frame_buffer = [color; WIDTH * HEIGHT];
+    let frame_buffer = if select_pressed {
+        if let Some(environ_cb) = ENVIRON_CB {
+            environ_cb(ENVIRONMENT_SHUTDOWN, std::ptr::null_mut());
+        }
+        [COLOR_WHITE; WIDTH * HEIGHT]
+    } else {
+        [COLOR_GREEN; WIDTH * HEIGHT]
+    };
 
     if let Some(video_cb) = VIDEO_CB {
         video_cb(
@@ -92,18 +108,13 @@ pub unsafe extern "C" fn retro_run() {
     }
 }
 
-// Required boilerplate functions
 #[no_mangle] pub unsafe extern "C" fn retro_init() {}
 #[no_mangle] pub unsafe extern "C" fn retro_deinit() {}
 #[no_mangle] pub unsafe extern "C" fn retro_unload_game() {}
 #[no_mangle] pub unsafe extern "C" fn retro_reset() {}
-#[no_mangle] pub unsafe extern "C" fn retro_set_audio_sample(_cb: unsafe extern "C" fn(i16, i16)) {}
-#[no_mangle] pub unsafe extern "C" fn retro_set_audio_sample_batch(_cb: unsafe extern "C" fn(*const i16, usize) -> usize) -> usize { 0 }
-#[no_mangle] pub unsafe extern "C" fn retro_set_input_poll(cb: unsafe extern "C" fn()) { INPUT_POLL = Some(cb); }
-#[no_mangle] pub unsafe extern "C" fn retro_set_input_state(cb: unsafe extern "C" fn(c_uint, c_uint, c_uint, c_uint) -> i16) { INPUT_STATE = Some(cb); }
 #[no_mangle] pub unsafe extern "C" fn retro_set_controller_port_device(_port: c_uint, _device: c_uint) {}
 
-#[no_mangle] 
+#[no_mangle]
 pub unsafe extern "C" fn retro_get_system_av_info(info: *mut c_void) {
     let info = info as *mut libretro_sys::SystemAvInfo;
     (*info).geometry.base_width = 256;
@@ -122,6 +133,7 @@ pub unsafe extern "C" fn retro_get_system_av_info(info: *mut c_void) {
 #[no_mangle] pub unsafe extern "C" fn retro_cheat_reset() {}
 #[no_mangle] pub unsafe extern "C" fn retro_cheat_set(_index: c_uint, _is_enabled: bool, _code: *const c_char) {}
 #[no_mangle] pub unsafe extern "C" fn retro_load_game_special(_game_type: c_uint, _info: *const c_void, _num_info: usize) -> bool { false }
+
 #[no_mangle] pub unsafe extern "C" fn retro_get_region() -> c_uint { 0 }
 #[no_mangle] pub unsafe extern "C" fn retro_get_memory_data(_id: c_uint) -> *mut c_void { std::ptr::null_mut() }
 #[no_mangle] pub unsafe extern "C" fn retro_get_memory_size(_id: c_uint) -> usize { 0 }
